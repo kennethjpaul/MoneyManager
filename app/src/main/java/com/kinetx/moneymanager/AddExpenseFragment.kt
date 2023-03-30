8e018de46e0641caf6cd3ec98388f24d3995e44f
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2023-03-31 01:11:22 +0200

    Added the option to pick time

--- app/src/main/java/com/kinetx/moneymanager/AddExpenseFragment.kt
+++ app/src/main/java/com/kinetx/moneymanager/AddExpenseFragment.kt
@@ -1,10 +1,14 @@
 package com.kinetx.moneymanager
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -13,6 +17,12 @@ import com.kinetx.moneymanager.databinding.FragmentAddExpenseBinding
 
 class AddExpenseFragment : Fragment() {
 
+    private val monthArray = arrayOf(
+        "Jan", "Feb",
+        "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    )
+
+    lateinit var binding: FragmentAddExpenseBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,21 +30,67 @@ class AddExpenseFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         (activity as AppCompatActivity).supportActionBar?.title = "Add Expense"
-        val binding : FragmentAddExpenseBinding  = DataBindingUtil.inflate(inflater, R.layout.fragment_add_expense, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_expense, container, false)
+
         binding.addExpenseAddCategoryBtn.setOnClickListener()
         {
-            val transactionType : String = "Expense"
-            val actionType : String ="category"
-            view?.findNavController()?.navigate(AddExpenseFragmentDirections.actionAddExpenseFragmentToSelectCategoryFragment(transactionType,actionType))
+            val transactionType: String = "Expense"
+            val actionType: String = "category"
+            view?.findNavController()?.navigate(
+                AddExpenseFragmentDirections.actionAddExpenseFragmentToSelectCategoryFragment(
+                    transactionType,
+                    actionType
+                )
+            )
         }
 
         binding.addExpenseAddAccountBtn.setOnClickListener()
         {
-            val transactionType : String = "Expense"
-            val actionType : String ="account"
-            view?.findNavController()?.navigate(AddExpenseFragmentDirections.actionAddExpenseFragmentToSelectCategoryFragment(transactionType,actionType))
+            val transactionType: String = "Expense"
+            val actionType: String = "account"
+            view?.findNavController()?.navigate(
+                AddExpenseFragmentDirections.actionAddExpenseFragmentToSelectCategoryFragment(
+                    transactionType,
+                    actionType
+                )
+            )
+        }
+
+        val myCalendar = Calendar.getInstance()
+
+        "${myCalendar.get(Calendar.DAY_OF_MONTH)} \n ${monthArray[myCalendar.get(Calendar.MONTH)]}\n${
+            myCalendar.get(
+                Calendar.YEAR
+            )
+        }".also { binding.addExpenseAddDateBtn.text = it }
+
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayofMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayofMonth)
+            updateDate(year, month, dayofMonth)
+        }
+
+        binding.addExpenseAddDateBtn.setOnClickListener()
+        {
+            DatePickerDialog(
+                it.context,
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
         return binding.root
+    }
+
+    private fun updateDate(year: Int, month: Int, dayofMonth: Int) {
+        Toast.makeText(activity, "date is ${year}-${month}-${dayofMonth}", Toast.LENGTH_SHORT)
+            .show()
+        "$dayofMonth \n ${monthArray[month]} \n$year".also {
+            binding.addExpenseAddDateBtn.text = it
+        }
     }
 
 
--- app/src/main/java/com/kinetx/moneymanager/AddIncomeFragment.kt
+++ app/src/main/java/com/kinetx/moneymanager/AddIncomeFragment.kt
@@ -1,10 +1,13 @@
 package com.kinetx.moneymanager
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
@@ -14,6 +17,12 @@ import com.kinetx.moneymanager.databinding.FragmentAddIncomeBinding
 
 class AddIncomeFragment : Fragment() {
 
+    lateinit var binding: FragmentAddIncomeBinding
+
+    private val monthArray = arrayOf(
+        "Jan", "Feb",
+        "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,21 +30,65 @@ class AddIncomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         (activity as AppCompatActivity).supportActionBar?.title = "Add Income"
-        val binding : FragmentAddIncomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_income, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_income, container, false)
+
         binding.addIncomeAddCategoryBtn.setOnClickListener()
         {
-            val transactionType : String = "income"
-            val actionType : String ="category"
-            view?.findNavController()?.navigate(AddIncomeFragmentDirections.actionAddIncomeFragmentToSelectCategoryFragment(transactionType,actionType))
+            val transactionType: String = "income"
+            val actionType: String = "category"
+            view?.findNavController()?.navigate(
+                AddIncomeFragmentDirections.actionAddIncomeFragmentToSelectCategoryFragment(
+                    transactionType,
+                    actionType
+                )
+            )
         }
 
         binding.addIncomeAddAccountBtn.setOnClickListener()
         {
-            val transactionType : String = "Expense"
-            val actionType : String ="account"
-            view?.findNavController()?.navigate(AddIncomeFragmentDirections.actionAddIncomeFragmentToSelectCategoryFragment(transactionType,actionType))
+            val transactionType: String = "Expense"
+            val actionType: String = "account"
+            view?.findNavController()?.navigate(
+                AddIncomeFragmentDirections.actionAddIncomeFragmentToSelectCategoryFragment(
+                    transactionType,
+                    actionType
+                )
+            )
         }
+
+        val myCalendar = Calendar.getInstance()
+
+        "${myCalendar.get(Calendar.DAY_OF_MONTH)} \n ${monthArray[myCalendar.get(Calendar.MONTH)]}\n${
+            myCalendar.get(
+                Calendar.YEAR
+            )
+        }".also { binding.addIncomeAddDateBtn.text = it }
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayofMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayofMonth)
+            updateDate(year, month, dayofMonth)
+        }
+
+        binding.addIncomeAddDateBtn.setOnClickListener()
+        {
+            DatePickerDialog(
+                it.context,
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         return binding.root
+    }
+
+    private fun updateDate(year: Int, month: Int, dayofMonth: Int) {
+        Toast.makeText(activity, "date is ${year}-${month}-${dayofMonth}", Toast.LENGTH_SHORT)
+            .show()
+        "$dayofMonth \n ${monthArray[month]} \n$year".also { binding.addIncomeAddDateBtn.text = it }
     }
 
 }
