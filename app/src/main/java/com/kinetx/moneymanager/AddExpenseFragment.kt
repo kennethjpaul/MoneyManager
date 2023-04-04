4ba0c707298db46a50a8a78180d0d4d1d364edec
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2023-04-05 00:48:57 +0200

    Implemented view model to add expense fragment

--- app/src/main/java/com/kinetx/moneymanager/AddExpenseFragment.kt
+++ app/src/main/java/com/kinetx/moneymanager/AddExpenseFragment.kt
@@ -12,34 +12,47 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.setFragmentResultListener
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.kinetx.moneymanager.databinding.FragmentAddExpenseBinding
 
 
-class AddExpenseFragment : Fragment() {
+class AddExpenseFragment : Fragment()
+{
 
-    private val monthArray = arrayOf(
-        "Jan", "Feb",
-        "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    )
 
     lateinit var binding: FragmentAddExpenseBinding
+    private lateinit var viewModel : AddExpenseViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
         // Inflate the layout for this fragment
         (activity as AppCompatActivity).supportActionBar?.title = "Add Expense"
+
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_expense, container, false)
+        viewModel = ViewModelProvider(this).get(AddExpenseViewModel::class.java)
+
+        binding.addExpenseViewModel = viewModel
+        binding.lifecycleOwner = this
 
         setFragmentResultListener("SelectCategory")
         {key, bundle ->
-            val varColor = bundle.getString("color")
+            val varBgColor = bundle.getInt("color")
             val varType = bundle.getString("category")
-            val varBg = bundle.getInt("bg")
+            val varImgId = bundle.getInt("imgId")
             val varId = bundle.getLong("id")
-            Toast.makeText(activity,"The $varType is updated an ID $varId and with bg : $varBg with $varColor ", Toast.LENGTH_LONG).show()
+
+            when(varType)
+            {
+                "account" -> viewModel.updateAccount(varId,varImgId,varBgColor)
+                "category" -> viewModel.updateCategory(varId,varImgId,varBgColor)
+            }
+
+            Toast.makeText(activity,"The $varType is updated an ID $varId and with bg : $varImgId with $varBgColor ", Toast.LENGTH_LONG).show()
         }
 
         binding.addExpenseAddCategoryBtn.setOnClickListener()
@@ -66,42 +79,21 @@ class AddExpenseFragment : Fragment() {
             )
         }
 
-        val myCalendar = Calendar.getInstance()
-
-        "${myCalendar.get(Calendar.DAY_OF_MONTH)} \n ${monthArray[myCalendar.get(Calendar.MONTH)]}\n${
-            myCalendar.get(
-                Calendar.YEAR
-            )
-        }".also { binding.addExpenseAddDateBtn.text = it }
-
-
-        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayofMonth ->
-            myCalendar.set(Calendar.YEAR, year)
-            myCalendar.set(Calendar.MONTH, month)
-            myCalendar.set(Calendar.DAY_OF_MONTH, dayofMonth)
-            updateDate(year, month, dayofMonth)
-        }
-
         binding.addExpenseAddDateBtn.setOnClickListener()
         {
-            DatePickerDialog(
-                it.context,
-                datePicker,
-                myCalendar.get(Calendar.YEAR),
-                myCalendar.get(Calendar.MONTH),
-                myCalendar.get(Calendar.DAY_OF_MONTH)
-            ).show()
+            viewModel.datePick(it)
+        }
+
+        viewModel.accountImageButton.observe(viewLifecycleOwner){ data ->
+            binding.addExpenseAddAccountBtn.setBackgroundResource(data.bgColor)
+            binding.addExpenseAddAccountBtn.setImageResource(data.imageId)
+        }
+
+        viewModel.categoryImageButton.observe(viewLifecycleOwner){data ->
+            binding.addExpenseAddCategoryBtn.setBackgroundResource(data.bgColor)
+            binding.addExpenseAddCategoryBtn.setImageResource(data.imageId)
         }
         return binding.root
     }
-
-    private fun updateDate(year: Int, month: Int, dayofMonth: Int) {
-        Toast.makeText(activity, "date is ${year}-${month}-${dayofMonth}", Toast.LENGTH_SHORT)
-            .show()
-        "$dayofMonth \n ${monthArray[month]} \n$year".also {
-            binding.addExpenseAddDateBtn.text = it
-        }
-    }
-
 
 }
