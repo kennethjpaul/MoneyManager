8a56c167fa122352685b585ecfb997aa332e2d4c
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2024-04-13 11:44:35 +0200

    Function: added the data to the monthly transcation page

--- app/src/main/java/com/kinetx/moneymanager/fragment/MonthlyTransactionFragment.kt
+++ app/src/main/java/com/kinetx/moneymanager/fragment/MonthlyTransactionFragment.kt
@@ -1,5 +1,6 @@
 package com.kinetx.moneymanager.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kinetx.moneymanager.R
 import com.kinetx.moneymanager.databinding.FragmentMonthlyTransactionBinding
+import com.kinetx.moneymanager.dataclass.TransactionChildList
+import com.kinetx.moneymanager.dataclass.TransactionParentList
+import com.kinetx.moneymanager.enums.TransactionType
+import com.kinetx.moneymanager.helpers.CommonOperations
+import com.kinetx.moneymanager.recyclerview.TransactionParentRV
 import com.kinetx.moneymanager.viewmodel.MonthlyTransactionViewModel
 import com.kinetx.moneymanager.viewmodelfactory.MonthlyTransactionViewModelFactory
 
 
-class MonthlyTransactionFragment : Fragment() {
+class MonthlyTransactionFragment : Fragment(), TransactionParentRV.TransactionParentListener {
 
     lateinit var binding : FragmentMonthlyTransactionBinding
     lateinit var viewModel: MonthlyTransactionViewModel
@@ -32,8 +40,36 @@ class MonthlyTransactionFragment : Fragment() {
         binding.monthlyTransactionViewmodel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        val adapter = TransactionParentRV(this)
+        binding.monthlyTransactionsRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.monthlyTransactionsRecyclerView.setHasFixedSize(true)
+        binding.monthlyTransactionsRecyclerView.adapter = adapter
+
+
+        viewModel.listRoomDatabase.observe(viewLifecycleOwner)
+        {
+            val data = it.groupBy { it.date }.map {
+                TransactionParentList(it.key, it.value.map {
+                    TransactionChildList(it.transactionId,it.categoryOne,it.categoryTwo,when(it.transactionType)
+                    {
+                        TransactionType.TRANSFER -> Color.parseColor("#FFa4c639")
+                        TransactionType.INCOME -> Color.parseColor("#FF5d8aa8")
+                        TransactionType.EXPENSE -> Color.parseColor("#FF970203")
+                        else-> Color.parseColor("#FFa4c639")
+                    },it.comments,it.amount,
+                        CommonOperations.getResourceInt(application,it.categoryImageString),it.categoryColor,it.transactionType)
+                }, it.value.filter { it.transactionType==TransactionType.EXPENSE }.map { it.amount }.sum() - it.value.filter { it.transactionType==TransactionType.INCOME }.map { it.amount }.sum() )
+            }.sortedByDescending {
+                it.date
+            }
+            adapter.setData(data)
+        }
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onLongClickTransactionParent(position: Long, transactionType: TransactionType) {
+        view?.findNavController()?.navigate(MonthlyTransactionFragmentDirections.actionMonthlyTransactionFragmentToAddTransactionFragment(transactionType,position))
     }
 
 }
--- app/src/main/java/com/kinetx/moneymanager/viewmodel/MonthlyTransactionViewModel.kt
+++ app/src/main/java/com/kinetx/moneymanager/viewmodel/MonthlyTransactionViewModel.kt
@@ -1,8 +1,70 @@
 package com.kinetx.moneymanager.viewmodel
 
 import android.app.Application
+import android.icu.util.Calendar
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.preference.PreferenceManager
+import com.kinetx.moneymanager.database.DatabaseMain
+import com.kinetx.moneymanager.database.DatabaseRepository
+import com.kinetx.moneymanager.dataclass.TransactionListClass
+import com.kinetx.moneymanager.helpers.DateManipulation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MonthlyTransactionViewModel(application: Application): AndroidViewModel(application) {
+
+
+    private val sp = PreferenceManager.getDefaultSharedPreferences(getApplication())
+    private val startOfMonth : Int = sp.getString("startDayOfMonth","1")!!.toInt()
+    private var weekendEnabled : Boolean = sp.getBoolean("weekendSwitch",false);
+    private var weekendShift : Int = sp.getString("weekendPref","0")?.toInt() ?: 0
+
+
+    var myCalendarEnd: Calendar = Calendar.getInstance()
+    var myCalendarStart : Calendar = Calendar.getInstance()
+
+    private val _startDate = MutableLiveData<String>()
+    val startDate : LiveData<String>
+        get() = _startDate
+
+    private val _endDate = MutableLiveData<String>()
+    val endDate : LiveData<String>
+        get() = _endDate
+
+    var listRoomDatabase = MutableLiveData<List<TransactionListClass>>()
+
+    private val repository : DatabaseRepository
+
+    init {
+
+        val userDao = DatabaseMain.getInstance(application).databaseDao
+        repository = DatabaseRepository(userDao)
+
+        var myCalendar : Calendar = Calendar.getInstance()
+        myCalendar = DateManipulation.resetToMidnight(myCalendar)
+
+        myCalendarStart = DateManipulation.getStartOfMonth(myCalendar,startOfMonth, weekendEnabled,weekendShift)
+        myCalendarEnd = myCalendar.clone() as Calendar
+
+        _startDate.value = DateManipulation.getDateArray(myCalendarStart)
+        _endDate.value = DateManipulation.getDateArray(myCalendar)
+
+        updateTransactions()
+    }
+
+    private fun updateTransactions() {
+        viewModelScope.launch(Dispatchers.IO)
+        {
+            listRoomDatabase.postValue(
+                repository.getAllTransactions(
+                    myCalendarStart.timeInMillis,
+                    myCalendarEnd.timeInMillis
+                )
+            )
+        }
+    }
 
 }
