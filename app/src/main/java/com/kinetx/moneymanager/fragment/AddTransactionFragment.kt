0f774f63db4a699661287e2a54d62093f86587ac
Kenneth J Paul <kenneth.josephpaul@protonmail.com>
2024-07-22 22:12:56 +0200

    UI: Minor changes to make it compliant to android accessibility requirements

--- app/src/main/java/com/kinetx/moneymanager/fragment/AddTransactionFragment.kt
+++ app/src/main/java/com/kinetx/moneymanager/fragment/AddTransactionFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.setFragmentResultListener
